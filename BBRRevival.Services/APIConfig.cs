8cc5e2a2bc240bd6704a6e8b8e35c810ecf5b4a6
DodoNickey <dodonickey@gmail.com>
2024-11-01 16:52:54 +0000

    Revert "Added Stuff like Hats, Trails, news feed, teams (partially working, you cant join a team), fully upgraded Dirtbike and Offroader, freindlist, leaderboard (partially works) and Chat (its broken)."
    This reverts commit 7fd8c44c10292f8dedbc43e383fd16bdfb960d6e.

--- BBRRevival.Services/APIConfig.cs
+++ BBRRevival.Services/APIConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Net;
 
 namespace BBRRevival.Services;
-//you will also have to modify serverservice.cs' ip
+
 public class APIConfig
 {
     public string IP = "http://192.168.1.7:4451/";
