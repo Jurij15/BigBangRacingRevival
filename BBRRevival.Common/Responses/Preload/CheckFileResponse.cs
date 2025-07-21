94bbdc4b7b67bf73d23a9221f34dd90098c94f8d
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2025-07-21 22:28:03 +0200

    add assets, add checkFile endpoint

---
+++ BBRRevival.Common/Responses/Preload/CheckFileResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBRRevival.Common.Responses.Preload
+{
+    public class CheckFileResponse
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public string path { get; set; }
+        public int version { get; set; }
+    }
+}
--- BBRRevival.Server/Controllers/PreloadController.cs
+++ BBRRevival.Server/Controllers/PreloadController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BBRRevival.Common.Responses.Preload;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BBRRevival.Server.Controllers
 {
@@ -17,7 +20,42 @@ namespace BBRRevival.Server.Controllers
         public async Task<IActionResult> checkVersion()
         {
             _logger.LogInformation("Returning checkVersion");
-            return Ok(new { version = "upToDate"}); //TODO: add an actuall version check
+
+            CheckVersionResponse response = new()
+            {
+                version = "upToDate"
+            };
+
+            return Ok(response); //TODO: add an actuall version check
+        }
+
+        [HttpGet("checkFile")]
+        public async Task<IActionResult> checkFile()
+        {
+            _logger.LogInformation("Returning checkFile");
+
+            var request = HttpContext.Request;
+            string baseUrl = $"{request.Scheme}://{request.Host}";
+
+            string name = Request.QueryString.ToString().Split("&")[1].Remove(0, 5);
+
+            CheckFileResponse response = new()
+            {
+                name = name,
+                type = "music",
+                path = baseUrl += $"/v1/preload/downloadFile?{name}",
+                version = 0
+            };
+
+            return Ok(response);
+
+        }
+
+        [HttpGet("downloadFile")]
+        public async Task<IActionResult> downloadFile()
+        {
+            _logger.LogInformation("Returning downloadFile");
+            return Ok();
         }
     }
 }
