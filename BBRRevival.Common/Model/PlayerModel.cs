beb3fc3513bc324ca22b555f70f0cace2c6be44b
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2025-07-22 20:56:32 +0200

    add more playermodel properties

--- BBRRevival.Common/Model/PlayerModel.cs
+++ BBRRevival.Common/Model/PlayerModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,55 @@ namespace BBRRevival.Common.Model
         [Key]
         public Guid Id { get; set; } = new();
 
+        public string Name { get; set; }
+        public string Tag { get; set; }
+
+        public bool IsCheater { get; set; } = false; //default to false
+        public bool IsDeveloper { get; set; } = false; //default to false
+
+        public string GameCenterId { get; set; } //id for the ios gamecenter, to restore data
+        public string FacebookId { get; set; } //id for facebook, probably for sharing things on it
+
+        public string NinjaCreationTimestamp { get; set; } //i have no idea what this is
+
+        public string CountryCode { get; set; } //country code for the flag
+
+        public string YoutubeName { get; set; } //name of the youtube channel
+        public string YoutubeId { get; set; }
+        public int YoutubeSubscriberCount { get; set; }
+
+        public int ItemDbVersion { get; set; } = 0; //version of the database this entry uses, just default to 0 in this case
+
+        public int Coins { get; set; }
+
+        public int Copper { get; set; }
+
+        public int Diamonds { get; set; }
+
+        public int Shards { get; set; }
+
+        public int Stars { get; set; }
+
+        public int McBoosters { get; set; }
+
+        public int MaxMcBoosters { get; set; }
+
+        public int TournamentBoosters { get; set; }
+
+        public int CarBoosters { get; set; }
+
+        public int MaxCarBoosters { get; set; }
+
+        public int ItemLevel { get; set; }
+
+        public int Level { get; set; }
+
+        public int Cups { get; set; }
+
+        //client config things
         public Guid ClientConfigID { get; set; }
+
+        [ForeignKey(nameof(ClientConfigID))]
+        public ClientConfigModel ClientConfig { get; set; }
     }
 }
---
+++ BBRRevival.Server/Migrations/20250722185531_ImprovePlayer.cs
@@ -0,0 +1,226 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BBRRevival.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImprovePlayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Coins",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Copper",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "Players",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Diamonds",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FacebookId",
+                table: "Players",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GameCenterId",
+                table: "Players",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCheater",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeveloper",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ItemDbVersion",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Players",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NinjaCreationTimestamp",
+                table: "Players",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Shards",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Stars",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tag",
+                table: "Players",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "YoutubeId",
+                table: "Players",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "YoutubeName",
+                table: "Players",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "YoutubeSubscriberCount",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_ClientConfigID",
+                table: "Players",
+                column: "ClientConfigID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_ClientConfigs_ClientConfigID",
+                table: "Players",
+                column: "ClientConfigID",
+                principalTable: "ClientConfigs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_ClientConfigs_ClientConfigID",
+                table: "Players");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_ClientConfigID",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Coins",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Copper",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Diamonds",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "FacebookId",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "GameCenterId",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "IsCheater",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeveloper",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "ItemDbVersion",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "NinjaCreationTimestamp",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Shards",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Stars",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Tag",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "YoutubeId",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "YoutubeName",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "YoutubeSubscriberCount",
+                table: "Players");
+        }
+    }
+}
